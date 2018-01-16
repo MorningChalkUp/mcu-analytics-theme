1898b8451b7df95459276e225b836b9649ad072e
Matthew Masoni <matthewmasoni@Mats-MBP.local>
2018-01-15 18:53:55 -0800

    single report layout changes

--- templates/post_types/report-single.php
+++ templates/post_types/report-single.php
@@ -8,38 +8,51 @@
       <a href="/">&larr; Back to Reports</a>
     </div>
     <h2 class="section-title">
-      <?php //$logo = get_field('logo','user_'.$sponsor['ID']); ?>
-      <!-- <img src="<?php //echo $logo['url'] ?>" width="100" height="100"/> -->
       <?php pxl::image("acf|logo|user_{$sponsor['ID']}", array( 'w' => 100, 'h' => '100' )); ?>
-      <small><strong><?php the_field('date') ?></strong> Report</small>
+      <small><?php the_field('date') ?></small><br>
+      <em>&ldquo;<?php echo $mcu_email_data['subject'] ?>&rdquo;</em>
     </h2>
-    <hr>
+    
+    <br>
+    
+    <div class="stats">
+      
+      <div class="stat">
+        <label>Recipients</label>
+        <span class="num"><?php echo number_format($mcu_email_data['recipients']) ?></span>
+      </div>
+      <div class="stat">
+        <label>Opens</label>
+        <span class="num"><?php echo number_format($mcu_email_data['opens']) ?></span>
+      </div>
+      <div class="stat">
+        <label>Open Rate</label>
+        <span class="num"><?php  echo (round(($mcu_email_data['opens']/$mcu_email_data['recipients']*10000))/100).'%' ?></span>
+      </div>
+      <div class="stat">
+        <label>Clicks</label>
+        <span class="num"><?php echo number_format($mcu_email_data['ad_clicks']) ?></span>
+      </div>
+      
+      
+    </div>
+    
+    <br>
+    
     <div class="row">
-      <div class="span8">
-        <?php //fn::put($mcu_email_data); ?>
-        <div class="row">
-          <div class="span4">
-            <label>Recipients</label>
-            <span class="num"><?php echo number_format($mcu_email_data['recipients']) ?></span>
-          </div>
-          <div class="span4">
-            <label>Opens</label>
-            <span class="num"><?php echo number_format($mcu_email_data['opens']) ?></span>
-            
-          </div>
-        </div>
-        <br>
-        <div class="row">
-          <div class="span4">
-            <label>Clicks</label>
-            <span class="num"><?php echo number_format($mcu_email_data['ad_clicks']) ?></span>
-          </div>
-          <div class="span4">
-            <label>Open Rate</label>
-            <span class="num"><?php  echo (round(($mcu_email_data['opens']/$mcu_email_data['recipients']*10000))/100).'%' ?></span>
-          </div>
-        </div>
-        <hr>
+      
+      <div class="span6">
+        <h4>Ad Copy:</h4>
+        <?php the_field('ad_copy'); ?>
+        <p class="right-text"><a href="<?php echo $mcu_email_data['web_view'] ?>" target="popup" onclick="window.open('<?php echo $mcu_email_data['web_view'] ?>','popup','width=700,height=700,resizable=no'); return false;">View Full Newsletter &rarr;</a></p>
+        
+        <?php if($notes = get_field('notes')) :?>
+          <h4>Notes:</h4>
+          <?php echo  $notes; ?>
+        <?php endif; ?>
+      </div>
+      
+      <div class="span6">
         <h4>Click Activity by Link:</h4>
         <table class="fix">
         <?php
@@ -53,26 +66,9 @@
           endforeach;
         ?>
         </table>
-        <?php //fn::put($mcu_email_data) ?>
       </div>
-      <div class="span1 placeholder"></div>
-      <div class="span4">
-        <h4><em>&ldquo;<?php echo $mcu_email_data['subject'] ?>&rdquo;</em></h4>
-        <div class="ad-copy">
-          <?php the_field('ad_copy'); ?>
-        </div>
-        <?php /*
-        <div class="preview">
-          <iframe src="<?php echo $mcu_email_data['web_view'] ?>" width="600" height="900">
-          </iframe>
-        </div>
-        */ ?>
-        <h4 class="center-text"><a href="<?php echo $mcu_email_data['web_view'] ?>" target="popup" onclick="window.open('<?php echo $mcu_email_data['web_view'] ?>','popup','width=700,height=700,resizable=no'); return false;">View Full Newsletter</a></h4>
-        <?php if($notes = get_field('notes')) :?>
-          <h4>Notes:</h4>
-          <?php echo  $notes; ?>
-        <?php endif; ?>
-      </div>
+      
     </div>
+
   </div>
 </div>
