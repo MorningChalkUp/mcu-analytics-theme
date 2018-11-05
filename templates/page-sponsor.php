a5a20aef19dcb2d9aa595e35532a1eaa1275d320
Eric Sherred <eric@ericsherred.com>
2018-11-05 12:52:01 -0600

    logged out state and hide items and months with nothing for sale

--- templates/page-sponsor.php
+++ templates/page-sponsor.php
@@ -40,41 +40,53 @@
           }
           foreach($months as $key=>$month):
             ?>
-            <div class="month">
-              <h4 class="label"><?php echo $key ?></h4>
-              <?php foreach ($month as $week) : ?>
-                <?php
-                  $start = strtotime($week['start']);
-                  $end = strtotime($week['end']);
-                  $class = '';
-                  if ( date('M j',$start) == date('M j',$end) ){
-                    $range = date('M',$start).' '.date('j',$start);
-                    $class = 'single-day';
-                  } else if ( date('M',$start) == date('M',$end) ){
-                    $range = date('M',$start).' '.date('j',$start).' - '.date('j',$end);
-                  } else {
-                    $range = date('M',$start).' '.date('j',$start).' - '.date('M',$end).' '.date('j',$end);
-                  }
-                  
-                  if($week['availability'] == 'available'){
-                    $tooltip = false;
-                    $disabled = '';
-                  } else {
-                    $tooltip = $week['availability'];
-                    $disabled = 'disabled';
-                  }
-                  
-                  if( $week['purchaser']['ID'] == $user->ID ) {
-                    $purchaser = 'purchaser';
-                  } else {
-                    $purchaser = '';
-                  }
+            <?php 
+              foreach ($month as $week) {
+                $empty = true;
+                if ( $week['availability'] != 'not_for_sale' ) {
+                  $empty = false;
+                }
+              }
+            ?>
+            <?php if ( !$empty ): ?>
+              <div class="month">
+                <h4 class="label"><?php echo $key ?></h4>
+                <?php foreach ($month as $week) : ?>
+                  <?php if ( $week['availability'] != 'not_for_sale' ): ?>
+                    <?php
+                      $start = strtotime($week['start']);
+                      $end = strtotime($week['end']);
+                      $class = '';
+                      if ( date('M j',$start) == date('M j',$end) ){
+                        $range = date('M',$start).' '.date('j',$start);
+                        $class = 'single-day';
+                      } else if ( date('M',$start) == date('M',$end) ){
+                        $range = date('M',$start).' '.date('j',$start).' - '.date('j',$end);
+                      } else {
+                        $range = date('M',$start).' '.date('j',$start).' - '.date('M',$end).' '.date('j',$end);
+                      }
+                      
+                      if($week['availability'] == 'available'){
+                        $tooltip = false;
+                        $disabled = '';
+                      } else {
+                        $tooltip = $week['availability'];
+                        $disabled = 'disabled';
+                      }
 
-                ?>
-                <input <?php echo $disabled ?> class="purchase-checkbox <?php echo $purchaser ?>" id="<?php echo $start ?>" type="checkbox" data-id="<?php echo $start ?>" data-status="<?php echo $week['availability'] ?>" data-price="<?php echo $week['price'] ?>" data-range="<?php echo $range ?>" data-notes="<?php echo $week['notes'] ?>" data-start="<?php echo date('n/j/Y', strtotime($week['start'])) ?>" data-end="<?php echo date('n/j/Y', strtotime($week['end'])) ?>" />
-                <label class="<?php echo $class ?> purchase-checklabel" for="<?php echo $start ?>"><?php echo $range ?> <span><?php echo '$'.$week['price'] ?></span></label>
-              <?php endforeach; ?>
-            </div>
+                      if( $user != null && $week['purchaser']['ID'] == $user->ID ) {
+                        $purchaser = 'purchaser';
+                      } else {
+                        $purchaser = '';
+                      }
+
+                    ?>
+                    <input <?php echo $disabled ?> class="purchase-checkbox <?php echo $purchaser ?>" id="<?php echo $start ?>" type="checkbox" data-id="<?php echo $start ?>" data-status="<?php echo $week['availability'] ?>" data-price="<?php echo $week['price'] ?>" data-range="<?php echo $range ?>" data-notes="<?php echo $week['notes'] ?>" data-start="<?php echo date('n/j/Y', strtotime($week['start'])) ?>" data-end="<?php echo date('n/j/Y', strtotime($week['end'])) ?>" />
+                    <label class="<?php echo $class ?> purchase-checklabel" for="<?php echo $start ?>"><?php echo $range ?> <span><?php echo '$'.$week['price'] ?></span></label>
+                  <?php endif; ?>
+                <?php endforeach; ?>
+              </div>
+            <?php endif; ?>
             <?php
           endforeach;
         ?>
