e6efbfed90902b8cf070d54da05435ce9a65bc62
Matthew Masoni <matthewmasoni@Mats-MBP.local>
2018-11-05 10:45:03 -0800

    made sponsor content dynamic

--- templates/page-sponsor.php
+++ templates/page-sponsor.php
@@ -25,9 +25,7 @@
       <div id="products">
         <h3><i class="far fa-calendar-alt"></i></h3>
         <div>
-          <p>Below is a list of available advertising inventory on the Morning Chalk Up. Grayed out items have already been reserved. More inventory may be released at a later date.</p>
-          <p>For a full breakdown of current audience, the sponsorship program, and add on features download our 2019 sponsorship guide.</p>
-          <p>If you have any questions, please email <a href="mailto:justin@morningchalkup.com">Justin@morningchalkup.com</a>.</p>
+          <?php the_content(); ?>
           <hr>
         </div>
         
