413fe7cc37d612a706ce5ecc0e6599dfab020b33
Eric Sherred <eric@ericsherred.com>
2018-11-05 09:16:21 -0600

    updated functions

--- includes/class.theme.php
+++ includes/class.theme.php
@@ -14,21 +14,28 @@
             'orderby' => 'meta_value',
             'order' => 'DESC',
           ),
-        )
+        ),
+        'purchase' => 'dashicons-tag',
+        'purchased_item' => 'dashicons-cart',
       );
       
       $resources = array(
         'min' => false,
         'css' => array(
           'main'   => true,
+          'magnific-popup' => true,
           'gf-fix' => array('deps' => array('gforms_ready_class_css','gforms_formsmain_css')),
         ),
         'js'  => array(
+          'magnific-popup' => true,
           'fontawesome-all.min' => array('defer' => 'true'),
           'site' => array('deps' => array('jquery')),
         )
       );
     }
+    
+    
+    
     
     // Hooks : Actions
       public function actions() {
@@ -39,11 +46,27 @@
         add_action('wp_login_failed', array($this, 'action_wp_login_failed'), 10, 1);
         add_action('after_password_reset', array($this, 'action_after_password_reset'), 10, 1);
         
+        add_action( 'gform_user_registered', array($this, 'action_gf_registration_autologin'),  10, 4 );
+        
         add_action ('wp_loaded', array($this, 'update_sponsor_profile'));
         
         remove_action('wp_head', 'print_emoji_detection_script', 7);
         remove_action('wp_print_styles', 'print_emoji_styles');
       }
+      
+      function action_gf_registration_autologin( $user_id, $user_config, $entry, $password ) {
+       $user = get_userdata( $user_id );
+       $user_login = $user->user_login;
+       $user_password = $password;
+ 
+          wp_signon( array(
+       'user_login' => $user_login,
+       'user_password' =>  $user_password,
+       'remember' => false
+ 
+          ) );
+      }
+      
       public function action_init() {
         if ( !is_admin() ) {}
       }
@@ -56,28 +79,35 @@
       public function filters() {
         add_filter('gform_ajax_spinner_url', array($this, 'filter_gf_spinner_replace'), 10, 2 );
         add_filter('gform_enable_field_label_visibility_settings', '__return_true');
+        add_filter('acf/load_field/name=date', array($this, 'filter_acf_default_date') );
       }
+      
+      function filter_acf_default_date($field) {
+        $field['default_value'] = date('Ymd');
+        return $field;
+      }
+      
       public function filter_gf_spinner_replace( $image_src, $form ) {
         return  'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'; // relative to you theme images folder
       }
-			public function action_wp_login_failed( $username ) {
-				$referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-			   // if there's a valid referrer, and it's not the default log-in screen
-			   if ( !empty($referrer) && !strstr($referrer, 'wp-login') && !strstr($referrer, 'wp-admin') ) {
-			      wp_redirect( $referrer . '?login=failed' );
-			      exit;
-			   }
-			}
+      public function action_wp_login_failed( $username ) {
+        $referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+         // if there's a valid referrer, and it's not the default log-in screen
+         if ( !empty($referrer) && !strstr($referrer, 'wp-login') && !strstr($referrer, 'wp-admin') ) {
+            wp_redirect( $referrer . '?login=failed' );
+            exit;
+         }
+      }
       public function update_sponsor_profile() {
         if( is_user_logged_in() ) {
           require_once(THEME.'/includes/update-profile.php');
         }
       }
-			public function action_after_password_reset() {
-				// Redirects the user back to the My Account page once they have set a new password.
-				wp_redirect(home_url('/'));
-				exit;
-			}
+      public function action_after_password_reset() {
+        // Redirects the user back to the My Account page once they have set a new password.
+        wp_redirect(home_url('/'));
+        exit;
+      }
       
 
       
@@ -114,7 +144,11 @@
   
   // ACF Options
   if( function_exists('acf_add_options_page') ) {
-    acf_add_options_sub_page();
+    acf_add_options_sub_page(array(
+      'capability'  => 'manage_options',
+      'menu_title'  => 'Inventory Manager',
+      'page_title'  => 'Inventory Manager',
+    ));
   }
   
 }
