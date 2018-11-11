cc99a2401ccc9d8bc273e32929c0dd9f84ced828
Matthew Masoni <matthewmasoni@Mats-MBP.local>
2018-11-11 13:00:47 -0800

    purchase report styling

--- templates/page-report.php
+++ templates/page-report.php
@@ -44,91 +44,111 @@
     }
 
   ?>
-
-  <div class="section" style="padding-top: 1rem;">
-    <div class="wrapper">
-      <div><h2>Total Purchased: $<?php echo number_format($total, 2,'.', ',') ?></h2></div>
-      <div><h2>Total Paid: $<?php echo number_format($paid, 2,'.', ',') ?></h2></div>
-      <div><h2>Weeks Purchased: <?php echo $purchased_weeks ?>/<?php echo $available_weeks; ?> (<?php echo $total_weeks ?> Total)</h2></div>
-      <div><h2>Weekends Purchased: <?php echo $purchased_weekends ?>/<?php echo $available_weekends; ?> (<?php echo $total_weekends ?> Total)</h2></div>
-    </div>
-  </div>
-
   <div class="section">
     <div class="wrapper">
-      <hr>
-      <h1>Purchases By Week</h1>
-      <?php foreach ($weeks as $week): ?>
-        <h3 style="font-weight: 400">
-          <?php 
-            echo date('F j', strtotime($week['start'])); 
-            if ($week['start'] != $week['end']) {
-              echo ' - ' . date('F j', strtotime($week['end'])); 
-            }
-          ?>
-
-          <?php if ($week['purchaser']): ?>
-            - <strong style="font-weight: 700"><?php echo $week['purchaser']['display_name'] ?></strong>
-          <?php endif; ?>
-
-        </h3>
-      <?php endforeach ?>
-    </div>
-  </div>
-
-
-  <div class="section">
-    <div class="wrapper">
-      <hr>
-      <h1>Purchases By Order</h1>
-    </div>
-  </div>
-  <?php foreach ($purchases as $purchase): ?>
-    <div class="section">
-      <div class="wrapper">
-        <h2><?php echo get_field('purchaser', $purchase)['display_name']; ?> - Order#: <?php echo $purchase->ID; ?>
-          <br><small>Payment: <?php the_field( 'stripe_id', $purchase ); ?></small>
-        </h2>
-
-        <div style="font-weight: 700;">Total: $<?php echo number_format(get_field( 'purchase_total', $purchase ), 2,'.', ',') ?><br>
-          Paid: $<?php echo number_format(get_field( 'amount_paid', $purchase ), 2,'.', ',') ?>
+      
+      <div class="box">
+        <div class="stats">
+          <div class="stat">
+            <h4 class="label">Total Purchased:</h4>
+            <span class="num">$<?php echo number_format($total, 2,'.', ',') ?></span>
+          </div>
+          <div class="stat">
+            <h4 class="label">Total Paid:</h4>
+            <span class="num">$<?php echo number_format($paid, 2,'.', ',') ?></span>
+          </div>
+          <div class="stat">
+            <h4 class="label">Weeks Purchased:</h4>
+            <span class="num"><?php echo $purchased_weeks ?>/<?php echo $available_weeks; ?> </span>
+            <h4 class="label"><?php echo $total_weeks ?> Total</h4>
+          </div>
+          <div class="stat">
+            <h4 class="label">Weekends Purchased:</h4>
+            <span class="num"><?php echo $purchased_weekends ?>/<?php echo $available_weekends; ?> </span>
+            <h4 class="label"><?php echo $total_weekends ?> Total</h4>
+          </div>
         </div>
-        <?php 
-          $items = query_posts(array(
-            'post_type' => 'purchased_item',
-            'posts_per_page' => -1,
-            'meta_key' => 'purchase_id',
-            'meta_value' => $purchase->ID,
-            'orderby' => 'title',
-            'order' => 'ASC',
-          ));
-        ?>
-        <?php foreach ($items as $item): ?>
-          <h3>
-            <?php 
-              echo date('F j', strtotime(get_field('start', $item))); 
-              if(get_field('start', $item) != get_field('end', $item)) {
-                echo ' - ';
-                echo date('F j', strtotime(get_field('end', $item))); 
-              }
-            ?>   
-          </h3>
-          <?php if(get_field('facebook_retargeting', $item) || get_field('ab_testing', $item) || get_field('we_write_ads', $item)): ?>
-            <ul>
-              <?php if(get_field('facebook_retargeting', $item)): ?>
-                <li>Facebook Retargeting</li>
-              <?php endif; ?>
-              <?php if(get_field('ab_testing', $item)): ?>
-                <li>A/B Testing</li>
-              <?php endif; ?>
-              <?php if(get_field('we_write_ads', $item)): ?>
-                <li>We Write Ads</li>
-              <?php endif; ?>
-            </ul>
-          <?php endif; ?>
-          <hr>
-        <?php endforeach; ?>
       </div>
-    </div>
-  <?php endforeach; ?>
+      
+      <div class="row">
+        <div class="span8">
+          <h2>Purchases By Order</h2>
+
+          <?php foreach ($purchases as $purchase): ?>
+          <div class="box">
+            <h3 style="margin:0">
+              <?php echo get_field('purchaser', $purchase)['display_name']; ?> - Order#: <?php echo $purchase->ID; ?>
+              <br><small>Payment: <?php the_field( 'stripe_id', $purchase ); ?></small>
+            </h3>
+        
+            <table>
+              <tr style="border:none">
+                <td valign="top">
+                  <p>Total: $<?php echo number_format(get_field( 'purchase_total', $purchase ), 2,'.', ',') ?><br>
+                  Paid: $<?php echo number_format(get_field( 'amount_paid', $purchase ), 2,'.', ',') ?></p>
+                </td>
+                <td>
+                  <?php 
+                    $items = query_posts(array(
+                      'post_type' => 'purchased_item',
+                      'posts_per_page' => -1,
+                      'meta_key' => 'purchase_id',
+                      'meta_value' => $purchase->ID,
+                      'orderby' => 'title',
+                      'order' => 'ASC',
+                    ));
+                  ?>
+                  <table>
+                    <?php foreach ($items as $item): ?>
+                      <tr>
+                        <td>
+                          <?php 
+                            echo date('F j', strtotime(get_field('start', $item))); 
+                            if(get_field('start', $item) != get_field('end', $item)) {
+                              echo ' - ';
+                              echo date('F j', strtotime(get_field('end', $item))); 
+                            }
+                          ?>   
+                        </td>
+                        <td>
+                          <?php 
+                            if(get_field('facebook_retargeting', $item) || get_field('ab_testing', $item) || get_field('we_write_ads', $item)){
+                              if(get_field('facebook_retargeting', $item)) echo "[Facebook Retargeting]"; 
+                              if(get_field('ab_testing', $item)) echo "[A/B Testing]";
+                              if(get_field('we_write_ads', $item)) echo "[We Write Ads]";
+                            } 
+                          ?>
+                        </td>
+                      </tr>
+                    <?php endforeach; ?>
+                  </table>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <?php endforeach; ?>
+        </div>
+        <div class="span4">
+          <h2>By Date</h2>
+          <table>
+          <?php foreach ($weeks as $week): ?>
+            <tr>
+              <td>
+                <?php 
+                  echo date('M j', strtotime($week['start'])); 
+                  if ($week['start'] != $week['end']) echo ' - ' . date('M j', strtotime($week['end'])); 
+                ?>
+              </td>
+              <td align="right">
+                <?php 
+                  if ($week['purchaser']) echo "<strong>{$week['purchaser']['display_name']}</strong>";
+                ?>
+              </td>
+            </tr>
+          <?php endforeach ?>
+          </table>
+        </div>
+      </div>
+      
+
 <?php endif; ?>
