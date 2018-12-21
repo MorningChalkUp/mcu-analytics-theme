468dd81de3762b4dec4ac82c8b14889f2028e789
Eric Sherred <eric@ericsherred.com>
2018-12-20 19:12:27 -0600

    update to manual receipt page

--- templates/page-send-receipt.php
+++ templates/page-send-receipt.php
@@ -1,26 +1,33 @@
 <?php /* Template Name: Manual Receipt */ ?>
 
 <?php 
-  if ( $_SERVER['REQUEST_METHOD'] == 'POST' && !empty( $_POST['action'] ) && $_POST['action'] == 'manual-receipt' ) {
+  if ( $_SERVER['REQUEST_METHOD'] == 'POST' && !empty( $_POST['action'] ) ) {
+    if( $_POST['action'] == 'manual-receipt' ) {
+      $url = 'http://data.morningchalkup.com/api/ads/receipt';
+    } elseif ( $_POST['action'] == 'manual-copy' ) {
+      $url = 'http://data.morningchalkup.com/api/ads/reminder/copy';
+    } elseif ( $_POST['action'] == 'manual-payment' ) {
+      $url = 'http://data.morningchalkup.com/api/ads/reminder/payment';
+    } 
 
-    $data = $_POST;
+    if (isset($url)) {
+      $data = $_POST;
 
-    $data['send_admin'] = false;
+      $data['send_admin'] = false;
 
-    $url = 'http://data.morningchalkup.com/api/ads/receipt';
+      $query = http_build_query($data);
 
-    $query = http_build_query($data);
+      $ch = curl_init();
 
-    $ch = curl_init();
+      curl_setopt($ch,CURLOPT_URL, $url);
+      curl_setopt($ch,CURLOPT_POST, count($data));
+      curl_setopt($ch,CURLOPT_POSTFIELDS, $query);
+      curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 
-    curl_setopt($ch,CURLOPT_URL, $url);
-    curl_setopt($ch,CURLOPT_POST, count($data));
-    curl_setopt($ch,CURLOPT_POSTFIELDS, $query);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-
-    $result = curl_exec($ch);
-    
-    $success = true;
+      $result = curl_exec($ch);
+      
+      $success = true;
+    }
   }
 ?>
 
@@ -37,7 +44,7 @@
   <div class="wrapper">
     <?php if(isset($success) && $success): ?>
       <div class='success'>
-        <?php echo get_field('purchaser', $data['purchase_id'])['display_name'] ?>: Order #: <?php echo $data['purchase_id'] ?> Email Sent
+        Email Sent
       </div>
     <?php endif; ?>
   </div>
@@ -46,20 +53,12 @@
 <div class="section">
   <div class="wrapper">
     <?php foreach ($purchases->posts as $post): ?>
-      <form method="POST">
-        <input type="hidden" value="manual-receipt" name="action">
-        <input type="hidden" value="<?php echo $post->ID ?>" name="purchase_id">
         <?php $purchaser = get_field('purchaser', $post->ID) ?>
         <div class="box">
           <h2>
             <?php echo $purchaser['display_name']; ?>: Order #: <?php echo $post->ID ?><br>
             <small><?php echo $purchaser['user_email']; ?></small>
           </h2>
-          <input type="hidden" value="<?php echo $purchaser['display_name']; ?>" name="user[name]">
-          <input type="hidden" value="<?php echo $purchaser['user_email']; ?>" name="user[email]">
-          <input type="hidden" value="<?php the_field('stripe_id', $post->ID) ?>" name="transaction">
-          <input type="hidden" value="<?php the_field('purchase_total', $post->ID) ?>" name="total">
-          <input type="hidden" value="<?php the_field('amount_paid', $post->ID) ?>" name="paid">
 
           <?php 
             $items = query_posts(array(
@@ -73,26 +72,57 @@
           ?>
           <ul>
             <?php foreach ($items as $key => $week): ?>
-              <input type="hidden" value="<?php echo get_field('facebook_retargeting', $week) ? 'true' : 'false' ?>" name="items[<?php echo $key ?>][facebook]">
-              <input type="hidden" value="<?php echo get_field('ab_testing', $week) ? 'true' : 'false' ?>" name="items[<?php echo $key ?>][ab]">
-              <input type="hidden" value="<?php echo get_field('we_write_ads', $week) ? 'true' : 'false' ?>" name="items[<?php echo $key ?>][wewrite]">
+              <?php
+                if($key == 0) {
+                  $first_ad = get_field('start', $week);
+                }
+              ?>
               <li>
-                <?php 
-                  echo date('F j', strtotime(get_field('start', $week)));
-                  if(get_field('start', $week) != get_field('end', $week)) {
-                    echo ' - ';
-                    echo date('F j', strtotime(get_field('end', $week))); 
-                  }
-                ?>
-                <input type="hidden" value="<?php echo date('F j', strtotime(get_field('start', $week))) ?>" name="items[<?php echo $key ?>][start]">
-                <input type="hidden" value="<?php echo date('F j', strtotime(get_field('end', $week))) ?>" name="items[<?php echo $key ?>][end]">
+                <form method="POST">
+                  <input type="hidden" value="manual-copy" name="action">
+                  <input type="hidden" value="<?php echo $purchaser['display_name']; ?>" name="user[name]">
+                  <input type="hidden" value="<?php echo $purchaser['user_email']; ?>" name="user[email]">
+                  <input type="hidden" value="<?php echo $week->ID ?>" name="week_id">
+                  <input type="hidden" value="<?php echo get_field('start', $week); ?>" name="ad_date">
+                  <button type="submit"  class="btn btn-sm btn-reminder">Reminder</button>
+                  <?php 
+                    echo date('F j', strtotime(get_field('start', $week)));
+                    if(get_field('start', $week) != get_field('end', $week)) {
+                      echo ' - ';
+                      echo date('F j', strtotime(get_field('end', $week))); 
+                    }
+                  ?>
+                </form>
               </li>
             <?php endforeach; ?>
           </ul>
+        <form method="POST" style="display:inline-block;">
+          <input type="hidden" value="manual-receipt" name="action">
+          <input type="hidden" value="<?php echo $post->ID ?>" name="purchase_id">
+          <input type="hidden" value="<?php the_field('amount_paid', $post->ID) ?>" name="paid">
+          <input type="hidden" value="<?php echo $purchaser['display_name']; ?>" name="user[name]">
+          <input type="hidden" value="<?php echo $purchaser['user_email']; ?>" name="user[email]">
+          <input type="hidden" value="<?php the_field('stripe_id', $post->ID) ?>" name="transaction">
+          <input type="hidden" value="<?php the_field('purchase_total', $post->ID) ?>" name="total">
+          <?php foreach ($items as $key => $week): ?>
+            <input type="hidden" value="<?php echo get_field('facebook_retargeting', $week) ? 'true' : 'false' ?>" name="items[<?php echo $key ?>][facebook]">
+            <input type="hidden" value="<?php echo get_field('ab_testing', $week) ? 'true' : 'false' ?>" name="items[<?php echo $key ?>][ab]">
+            <input type="hidden" value="<?php echo get_field('we_write_ads', $week) ? 'true' : 'false' ?>" name="items[<?php echo $key ?>][wewrite]">
+            <input type="hidden" value="<?php echo date('F j', strtotime(get_field('start', $week))) ?>" name="items[<?php echo $key ?>][start]">
+            <input type="hidden" value="<?php echo date('F j', strtotime(get_field('end', $week))) ?>" name="items[<?php echo $key ?>][end]">
+          <?php endforeach; ?>
+          <button type="submit" class="btn">Send Receipt</button>
+        </form>
 
-          <button type="submit" class="btn">Send Email</button>
-        </div>
-      </form>
+        <form method="POST" style="display:inline-block;">
+          <input type="hidden" value="manual-payment" name="action">
+          <input type="hidden" value="<?php echo $purchaser['display_name']; ?>" name="user[name]">
+          <input type="hidden" value="<?php echo $purchaser['user_email']; ?>" name="user[email]">
+          <input type="hidden" value="<?php echo $post->ID ?>" name="order_id">
+          <input type="hidden" value="<?php echo $first_ad ?>" name="ad_date">
+          <button type="submit" class="btn btn-reminder">Payment Reminder</button>
+        </form>
+      </div>
     <?php endforeach; ?>
   </div>
 </div>
